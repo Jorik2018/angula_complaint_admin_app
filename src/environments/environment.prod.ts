72820123ea00535e000fc064ca77b6e35367bde7
Erik Antonio Alarcon Pinedo <ealarconp@regionancash.gob.pe>
2023-01-25 12:05:32 -0500

    correct enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
 	title: 'Dv Environment Heading',
 	APP_OAUTH_CLIENT_ID:'OveCl8KbB1pBcy2huDQ15yYC',
 	API_KEY: '1234_API_KEY_5678',
-	APP_BASE_URL:'http://web.regionancash.gob.pe',
-	APP_OAUTH_URL:'http://web.regionancash.gob.pe/api/oauth'
+	APP_BASE_URL:'https://web.regionancash.gob.pe',
+	APP_OAUTH_URL:'https://web.regionancash.gob.pe/api/oauth'
 };
