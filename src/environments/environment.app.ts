054e7b04be2cafd9435035b43caabac5cf07045f
Roger Vergara <roger.jvs.08@gmail.com>
2023-01-27 18:10:22 -0500

    Cambios environment

---
+++ src/environments/environment.app.ts
@@ -0,0 +1,9 @@
+export const environment = {
+	production: true,
+	title: 'Dv Environment Heading',
+	APP_OAUTH_CLIENT_ID: 'OveCl8KbB1pBcy2huDQ15yYC',
+	API_KEY: '1234_API_KEY_5678',
+	// APP_BASE_URL: 'https://web.regionancash.gob.pe',
+	// APP_BASE_URL:'http://localhost:8000',
+	APP_OAUTH_URL: 'https://web.regionancash.gob.pe/api/oauth'
+};
