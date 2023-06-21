80e6d96ee0ec44a1b49894905ee381a2f2a8a7bc
Roger Vergara <roger.jvs.08@gmail.com>
2023-06-21 10:05:12 -0500

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
 	APP_OAUTH_CLIENT_ID: 'OveCl8KbB1pBcy2huDQ15yYC',
 	API_KEY: '1234_API_KEY_5678',
 	APP_BASE_URL: 'https://web.regionancash.gob.pe',
-	// APP_BASE_URL:'http://localhost:8000',
-	APP_OAUTH_URL: 'https://web.regionancash.gob.pe/api/oauth'
+  BASE_HREF: '/admin/complaint',
+  APP_OAUTH_URL: 'https://web.regionancash.gob.pe/api/oauth',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,16 +9,21 @@ export const environment = {
 	APP_OAUTH_CLIENT_ID:'WLRqjxzEQ7Zd9D7jCwiYjs28',
 	API_KEY: '1234_API_KEY_5678',
 	APP_BASE_URL:'http://web.regionancash.gob.pe',
-	// APP_BASE_URL:'http://localhost:8000',
-	APP_OAUTH_URL:'http://web.regionancash.gob.pe/api/oauth'
-   
+	// deploy local
+  // APP_BASE_URL:'http://localhost:8000',
+  // deploy web y local
+  BASE_HREF: '/admin/complaint',
+  // android
+  // BASE_HREF:'/',
+  APP_OAUTH_URL: 'https://web.regionancash.gob.pe/api/oauth',
+
 };
 
 /*
  var client_id = process.env.REACT_APP_OAUTH_CLIENT_ID;
       var oauth_url = process.env.REACT_APP_OAUTH_URL;
       window.location.href = `${oauth_url}/authorize?response_type=code&client_id=${client_id}&scope=profile`;
-    
+
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
